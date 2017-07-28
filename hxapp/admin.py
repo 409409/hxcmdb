dd7722bdaece7dc6ed90305c8dcc56cfc77cb460
phoenix <1047767505@qq.com>
2017-07-28 16:30:25 +0800

    修改admin.py

--- hxapp/admin.py
+++ hxapp/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Idc)
+admin.site.register(Host)
+admin.site.register(User)
+admin.site.register(Order)
 # Register your models here.
