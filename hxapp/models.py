2288d152df2fb83b96d748821b8b27ad6a62c72d
phoenix <1047767505@qq.com>
2017-07-28 16:44:03 +0800

    去除model里面的INT类似大小8的设定

--- hxapp/models.py
+++ hxapp/models.py
@@ -7,7 +7,7 @@ class Idc(models.Model):
     name = models.CharField(max_length=40, verbose_name=u'供应商名称')
     webstie = models.CharField(max_length=40, verbose_name=u'供应商网站')
     create_time = models.DateField(verbose_name=u'创建时间')
-    status = models.IntegerField(max_length=8, verbose_name=u'使用状态')
+    status = models.IntegerField(verbose_name=u'使用状态')
 
 class Host(models.Model):
     name = models.CharField(max_length=40, verbose_name=u'主机名称')
@@ -18,7 +18,7 @@ class Host(models.Model):
     idc_contact = models.CharField(max_length=40, verbose_name=u'主机供应商联系方式')
     create_time = models.DateField(verbose_name=u'创建时间')
     due_time = models.DateField(verbose_name=u'过期时间')
-    status = models.IntegerField(max_length=8, verbose_name=u'使用状态')
+    status = models.IntegerField(verbose_name=u'使用状态')
 
 class User(models.Model):
     name = models.CharField(max_length=40, verbose_name=u'用户名称')
@@ -27,7 +27,7 @@ class User(models.Model):
     departname = models.CharField(max_length=40, verbose_name=u'用户所在部门名称')
     email = models.CharField(max_length=40, verbose_name=u'用户电邮')
     register_time = models.DateField(verbose_name=u'创建用户时间')
-    status = models.IntegerField(max_length=8, verbose_name=u'使用状态')
+    status = models.IntegerField(verbose_name=u'使用状态')
     last_ip = models.CharField(max_length=40, verbose_name=u'用户最后登录IP')
     last_time = models.DateField(verbose_name=u'用户最后登录时间')
 
@@ -38,7 +38,7 @@ class Order(models.Model):
     bank_name = models.CharField(max_length=40, verbose_name=u'银行名称')
     start_time = models.DateField(verbose_name=u'申请时间')
     due_time = models.DateField(verbose_name=u'截止时间')
-    pay_status = models.IntegerField(max_length=8, verbose_name=u'付款状态')
-    examine_status = models.IntegerField(max_length=8, verbose_name=u'审核状态')
-    status = models.IntegerField(max_length=8, verbose_name=u'申请单状态')
+    pay_status = models.IntegerField(verbose_name=u'付款状态')
+    examine_status = models.IntegerField(verbose_name=u'审核状态')
+    status = models.IntegerField(verbose_name=u'申请单状态')
 # Create your models here.
